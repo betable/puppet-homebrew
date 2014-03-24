58e4c650ac8647f0ec532ec8e74c1a241f96d3ae
Richard Crowley <r@rcrowley.org>
2014-03-24 20:20:03 +0000

    Determine Xcode status via /Library/Developer.
    On Mavericks, and possibly even earlier, /usr/bin/cc and friends are always
    installed but just ask you (via a GUI, annoyingly) to install Xcode.  This
    makes automated installation difficult.
    
    Empirically, /Library/Developer doesn't seem to exist until the tools really
    are installed so we can safely use that to determine whether to install the
    Xcode Command Line Tools package.

--- lib/facter/has_compiler.rb
+++ lib/facter/has_compiler.rb
@@ -1,6 +1,6 @@
 Facter.add(:has_compiler) do
   confine :operatingsystem => :darwin
   setcode do
-    File.exists?('/usr/bin/cc') || system('/usr/bin/xcrun -find cc >/dev/null 2>&1')
+    File.exists?('/Library/Developer')
   end
 end
